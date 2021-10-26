c6721e623bbe808f0ab7c4c5358f9c215030a252
rasmuslarsson17 <rasmus.larsson@icloud.com>
2021-10-26 15:58:56 +0200

    initial commit

---
+++ Github.test.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Github.test._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
